b880ee05f3783ee322ec833473b1386388548ba4
Branderson Lima <braanlennon@gmail.com>
2020-05-18 12:12:00 -0300

    Add files via upload

---
+++ atividade6.js
@@ -0,0 +1,16 @@
+var Aluno1 = new Object();
+Aluno1.nome = prompt("Digite o nome do primeiro aluno:")
+Aluno1.RA = prompt("Digite o RA do primeiro aluno:")
+
+var Aluno2 = {
+    nome:"Nome Aluno",
+    RA: "RA Aluno"
+};
+
+var Aluno3 = {};
+Aluno3.nome = prompt("Digite o nome do terceiro aluno:")
+Aluno3.RA = prompt("Digite o RA do terceiro aluno:")
+
+alert("Aluno 1: " + Aluno1.nome + ". RA: " + Aluno1.RA)
+alert("Aluno 2: " + Aluno2.nome + ". RA: " + Aluno2.RA)
+alert("Aluno 3: " + Aluno3.nome + ". RA: " + Aluno3.RA)
