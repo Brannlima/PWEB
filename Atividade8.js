bbb4c0f54253652ae1430f0620ca080c426d4ee8
Branderson Lima <braanlennon@gmail.com>
2020-06-01 12:24:17 -0300

    Add files via upload

---
+++ Atividade8.js
@@ -0,0 +1,11 @@
+function janelaAberta () {
+   document.getElementById("janela").src = "./janelaaberta.jpg"
+}
+
+function janelaFechada() {
+    document.getElementById("janela").src = "./janelafechada.jpg"
+}
+
+function janelaQuebrada() {
+    document.getElementById("janela").src = "./janelaquebrada.jpg"
+}
